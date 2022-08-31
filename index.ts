0d5c683128b5e2e07a27b8b387bac6c13bae2e25
Jacob Andersen <jacob@algorithmjunkie.com>
2022-08-31 10:36:47 +0800

    No clue if this is going to work

--- index.ts
+++ index.ts
@@ -1,17 +1,59 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.153.0/http/server.ts";
+import MinecraftProtocol from "https://esm.sh/minecraft-protocol@1.36.1?target=deno&no-dts";
 
-interface SomeType {
-    message: string
+interface MCServerAddr {
+    hostname: string
+    port: number
 }
 
-const out: SomeType = {
-    message: "hi"
+class HttpResp {
+    body: Record<string, unknown>;
+    code: number;
+
+    constructor(body: Record<string, unknown> = { message: "No message provided" }, code: number = 200) {
+        this.body = body;
+        this.code = code;
+    }
+
+    build(): Response {
+        return new Response(JSON.stringify(this.body), {
+            status: this.code,
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+    }
 }
 
-serve((_req) => {
-    return new Response(JSON.stringify(out), {
-        headers: {
-            "content-type": "text/json"
-        }
-    })
+class Error extends HttpResp {
+    constructor(code: number = 500, message: string = "No message provided") {
+        super({ error: message }, code);
+    }
+}
+
+serve(async (req) => {
+    if (req.method !== 'GET') {
+        return new Error(400, "Method not allowed. Use GET.").build();
+    }
+
+    const data: URLSearchParams = new URL(req.url).searchParams;
+    if (!data.has("host") || !data.has("port")) {
+        return new Error(400, "Host and port must be specified in the query params.").build();
+    }
+
+    const host: string | null = data.get("host")
+    const port: string | null  = data.get("port")
+
+    if (!host || !port) {
+        return new Error(400, "Invalid values were received for the host and port of the target Minecraft server").build();
+    }
+
+    let pingResponse = null
+    try {
+        pingResponse = await MinecraftProtocol.ping({ host, port })
+    } catch (err) {
+        return new Error(500, `Encountered error during ping - ${err.name}: ${err.message}`).build();
+    }
+
+    return new HttpResp(pingResponse, 200).build();
 });
